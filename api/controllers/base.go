7f087882b36fcfb288c63c0b701bf4217e0d77ed
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-03-17 20:49:14 -0300

    Server running with properly

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -17,28 +17,15 @@ type Server struct {
 }
 
 func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
-
 	var err error
 
-	if Dbdriver == "mysql" {
-		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
-		server.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
-	}
-	if Dbdriver == "postgres" {
-		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
-		server.DB, err = gorm.Open(Dbdriver, DBURL)
-		if err != nil {
-			fmt.Printf("Cannot connect to %s database", Dbdriver)
-			log.Fatal("This is the error:", err)
-		} else {
-			fmt.Printf("We are connected to the %s database", Dbdriver)
-		}
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+	server.DB, err = gorm.Open(Dbdriver, DBURL)
+	if err != nil {
+		fmt.Println("Cannot connect to postgres database")
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Println("We are connected to the postgres database")
 	}
 
 	server.DB.Debug().AutoMigrate(&models.User{}, &models.Account{}, &models.Entry{}, &models.Transfer{}) //database migration
--- api/server.go
+++ api/server.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	"github.com/alejandrehl/simple-bank-api/api/controllers"
-	"github.com/alejandrehl/simple-bank-api/api/seed"
 	"github.com/joho/godotenv"
 )
 
@@ -22,7 +21,7 @@ func Run() {
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	seed.Load(server.DB)
+	// seed.Load(server.DB)
 
 	server.Run(":8080")
 }
