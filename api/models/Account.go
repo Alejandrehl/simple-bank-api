8bac6980ab87f1a794f62494518563c2b4fa9460
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-03-17 17:43:36 -0300

    user model

---
+++ api/models/Account.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type Account struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Owner    User      `json:"owner"`
+	OwnerID  uint32    `gorm:"not null" json:"owner_id"`
+	Balance  uint32    `gorm:"default:0;" json:"balance"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/alejandrehl/simple-bank-api
 
 go 1.17
+
+require (
+	github.com/badoux/checkmail v1.2.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+)
