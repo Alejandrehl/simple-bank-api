297fee2dfe7fdb437359f6c5183bb67d5b1f0a3c
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-03-18 01:04:05 -0300

    save

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -7,5 +7,5 @@ import (
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
+	responses.JSON(w, http.StatusOK, "Welcome to the Simple Bank API")
 }
